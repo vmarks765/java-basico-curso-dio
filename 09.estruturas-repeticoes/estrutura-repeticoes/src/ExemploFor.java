fc890d55bf77f8217564e53b1b1e029b79bac176
vmarks765 <vinicius.p.marques@live.com>
2023-04-26 22:20:40 -0300

    Atualizando pastas

---
+++ 09.estruturas-repeticoes/estrutura-repeticoes/src/ExemploFor.java
@@ -0,0 +1,15 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        /*
+        estrutura do controle de fluxo for
+        for (bloco de inicialização; expressão booleana de validação; bloco de atualização){
+            comando que será executado até que a
+            expressão de validação torne-se falsa
+        }
+        */
+
+        for(int carneirinhos = 1 ; carneirinhos <=20; carneirinhos ++) {
+            System.out.println(carneirinhos + " - Carneirinho(s)");
+        }
+    }
+}
